3c7cbda4e5cc9132d86d9037109765ed924c1df7
StevenCreates <steven@stevencreates.tech>
2020-11-25 07:47:13 -0700

    imported functions component into app

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Types from './components/Types'
+import Functions from './components/Functions';
 function App() {
   return (
     <div className="App">
 
         <Types />
-
+        <Functions />
     </div>
   );
 }
