282bc21776d8560c78bab5f5df97b4238f01c4c4
StevenCreates <steven@stevencreates.tech>
2020-12-02 09:05:46 -0700

    added button component to app

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Types from './components/Types'
 import Functions from './components/Functions';
 import { Head } from './components/Head';
+import { Button } from './components/Button';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Head title="Hello" />
       <Types />
       <Functions />
+      <Button onClick={() => console.log('hi ello')} />
     </div>
   );
 }
