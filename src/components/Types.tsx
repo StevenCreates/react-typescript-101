3d902d70bd2d8156ab87f6fc7cfde8697b8e56ba
StevenCreates <steven@stevencreates.tech>
2020-12-02 08:04:04 -0700

    added new type

--- src/components/Types.tsx
+++ src/components/Types.tsx
@@ -1,36 +1,37 @@
 import React from 'react'
 
 const Types = () => {
-    const isOpen : boolean = true;
-    const firstName : string = 'steven'
-    const lastName : string = 'hofheins'
-    const age : number = 30
-    const myArr : number[] = [0, 1, 2, 3]
+    const isOpen: boolean = true;
+    const firstName: string = 'steven'
+    const lastName: string = 'hofheins'
+    const age: number = 30
+    const myArr: number[] = [0, 1, 2, 3]
     const value = isOpen ? 'true' : 'false'
+    const spinner: 'Loading' | 'loaded'
 
     return (
         <div>
-            <span style={{fontWeight: 'bold'}}>
-            TypeScript Types
+            <span style={{ fontWeight: 'bold' }}>
+                TypeScript Types
             </span>
-        
+
             <div>
-               <span>Type String: </span>
-               <span>{firstName} {lastName}</span>
+                <span>Type String: </span>
+                <span>{firstName} {lastName}</span>
             </div>
             <div>
-               <span>Type Number: </span>
-               <span>{age}</span>
+                <span>Type Number: </span>
+                <span>{age}</span>
             </div>
             <div>
-               <span>Type Boolean: </span>
-               <span>{value}</span>
+                <span>Type Boolean: </span>
+                <span>{value}</span>
             </div>
             <div>
-               <span>Type Array: </span>
-               {myArr.map((arrValue) => (
-                   <span>{arrValue}</span>
-               ))}
+                <span>Type Array: </span>
+                {myArr.map((arrValue) => (
+                    <span>{arrValue}</span>
+                ))}
             </div>
         </div>
     )
