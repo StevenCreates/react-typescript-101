af706e0f57570a5cba89653faceb028d20fa1ce8
StevenCreates <steven@stevencreates.tech>
2020-12-10 16:16:35 -0700

    refs with typescript

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,8 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 
 export const Input = () => {
     const [name, setName] = useState<string | number>("");
+    // !null Read Only
+    const ref = useRef<HTMLInputElement>(null!)
+    console.log("ref", ref.current.value)
+
     return (
-        <input value={name} onChange={e => setName(e.target.value)} />
+        <input ref={ref} value={name} onChange={e => setName(e.target.value)} />
     )
 }
