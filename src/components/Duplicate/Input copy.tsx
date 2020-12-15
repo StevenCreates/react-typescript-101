9cf8a39c424a259f3429abdd6b3c8d1c42ba8bd3
StevenCreates <steven@stevencreates.tech>
2020-12-15 06:45:52 -0700

    useRef typescript

--- src/components/Duplicate/Input copy.tsx
+++ src/components/Duplicate/Input copy.tsx
@@ -4,7 +4,7 @@ export const Input = () => {
     const [name, setName] = useState<string | number>("");
     // !null Read Only
     const ref = useRef<HTMLInputElement>(null!)
-    console.log("ref", ref?.current?.value)
+    console.log("ref", ref.current.value)
 
     return (
         <input ref={ref} value={name} onChange={e => setName(e.target.value)} />
